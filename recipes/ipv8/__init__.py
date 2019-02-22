9948b0d601c3addd74747e6ca4548769e3f005a7
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2019-02-22 11:50:51 +0100

    Added libtorrent

--- recipes/ipv8/__init__.py
+++ recipes/ipv8/__init__.py
@@ -13,7 +13,7 @@ class LocalIPV8Recipe(PythonRecipe):
     url = 'git+https://github.com/Tribler/py-ipv8.git'
 
     depends = ['apsw', 'cryptography', 'libsodium', 'netifaces',
-               'python2', 'setuptools', 'twisted', 'lib2to3', 'libnacl'
+               'python2', 'setuptools', 'twisted', 'lib2to3', 'libnacl', 'libtorrent'
               ]
 
     patches = []
